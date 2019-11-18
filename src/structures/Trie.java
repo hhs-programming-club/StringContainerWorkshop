b80755a2efb86ec5c7a2b5adb752164b695855a9
Siyong <huangsy222@gmail.com>
2019-11-17 22:55:56 -0800

    Version 1

--- src/structures/Trie.java
+++ src/structures/Trie.java
@@ -5,7 +5,7 @@ public class Trie implements StringContainer
 	class TrieNode //Each node in the trie is a TrieNode
 	{
 		public boolean terminate;	//This represents whether the string associated with this node is actually in the trie, rather than just a temporary (but still important) node. (This is equivalent to coloring the node red instead of blue in the example)
-		public TrieNode child[];	//This array (of size 128) points to all of the children of this node
+		public TrieNode child[];	//This array (of size 128) points to all of the children of this node. These are the edges of the tree
 		public TrieNode(boolean terminate)		//Initialization
 		{
 			this.terminate = terminate;
@@ -34,12 +34,14 @@ public class Trie implements StringContainer
 	}
 	public boolean insert(String s)
 	{
-		//TODO insert
+		//TODO insert: Navigate to node corresponding to String s, creating new nodes when necessary
+		//	If this node is already in the container (terminate == true) return false, because you didn't insert the string; it was already there. Otherwise, set terminate to true and return true, because you were able to add this string to the container
 		return false;
 	}
 	public boolean erase(String s)
 	{
-		//TODO erase
+		//TODO erase: Navigate to node corresponding to String s. If the node hasn't been created, return false because you cannot erase a non-existent string
+		//	If you can reach the node, check if it's actually in the container. If so, remove it and return true, meaning you've sucessfully removed the string. Otherwise, return false because it was not in the container and thus could not be erased
 		return false;
 	}
 	public boolean check(String s)		//This is pre-implemented and commented for your convenience. Use this method to check whether insert & erase behave as expected
